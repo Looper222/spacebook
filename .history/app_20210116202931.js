23bee5aeee60f529372bc8bbb150d275fa539a71
Dawid <dawid.jm@interia.pl>
2021-01-16 20:33:33 +0100

    Updated dbURI

--- .history/app_20210116202931.js
+++ .history/app_20210116202931.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 // database connection
-const dbURI = 'mongodb+srv://spb-db-mod:auRBDGmo1FKBSilh@nodetuts.je9tx.mongodb.net/spacebook?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://<spb-db-mod>:<auRBDGmo1FKBSilh>@nodetuts.je9tx.mongodb.net/<spacebook>?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
--- .history/app_20210116203045.js
+++ .history/app_20210116203045.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.json());
 
 // database connection
-const dbURI = 'mongodb+srv://spb-db-mod:auRBDGmo1FKBSilh@nodetuts.je9tx.mongodb.net/spacebook?retryWrites=true&w=majority';
+//const dbURI = 'mongodb+srv://<spb-db-mod>:<auRBDGmo1FKBSilh>@nodetuts.je9tx.mongodb.net/<spacebook>?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://netusertut:bl4bl4c4r@nodetuts.je9tx.mongodb.net/node-auth?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
