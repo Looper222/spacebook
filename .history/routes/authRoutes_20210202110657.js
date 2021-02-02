af429134e0f4cef4caaf33e752d82332b23211a5
looper <dawid.jm123@gmail.com>
2021-02-02 15:06:09 +0100

    Added searching only by fname or surname

---
+++ .history/routes/authRoutes_20210202110657.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const authController = require('../controllers/authController');
+const searchController = require('../controllers/searchController');
+
+const router = Router();
+
+router.post('/signup', authController.signup_post);
+router.post('/login', authController.login_post);
+router.post('/search', searchController.search_user);
+
+module.exports = router;
