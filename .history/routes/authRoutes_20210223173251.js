fd42dbafd0a5ffec66c5b8d2cb1487dd30280cad
Dawid <dawid.jm@interia.pl>
2021-02-23 17:33:15 +0100

    minor corrections

--- .history/routes/authRoutes_20210223173251.js
+++ .history/routes/authRoutes_20210223173251.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const authController = require('../controllers/authController');
 const searchController = require('../controllers/searchController');
+const newSearchController = require('../controllers/newSearchController');
 
 const router = Router();
 
 router.post('/signup', authController.signup_post);
 router.post('/login', authController.login_post);
-router.post('/search', )
+router.post('/search', newSearchController.new_search_user);
+router.post('/oldSearch', searchController.search_user);
+router.post('/getUser', newSearchController.get_user);
 
 module.exports = router;
