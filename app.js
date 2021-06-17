131bdb5d725b846897348e6142d9c0b2dc0562a0
Dawid <dawid.jm@interia.pl>
2021-06-17 22:56:36 +0200

    Enabled cors

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// cors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const port = app.listen(process.env.PORT || 8080);
 
